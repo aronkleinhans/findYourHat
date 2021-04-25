010ea1a88c069e387aa67de829aadcc3c8c1e6b1
aronKleinhans <kleinhans.aron@gmail.com>
2021-04-25 13:00:32 +0200

    add movement & boundaries

--- main.js
+++ main.js
@@ -8,44 +8,73 @@ const pathCharacter = '*';
 class Field {
     constructor(grid){
     this.grid = grid;
-    this.curPos = [0, 0];
-    this.nextPos = [0, 0];
+    this._startPos = [3, 0];
+    this.curPos = this._startPos;
+    this.lastMove = '';
+    this.lastMoveValid = true;
     }
     
-    startNotice(){
+    start(){
         console.clear();
-        const notice = prompt('Welcome to Hatfinder! \n \n Use these for movement: \n w - up \n a - left \n s - down \n d - right \n \n press enter to continue...')
+        const notice = prompt('Welcome to Hatfinder! \n \nUse these for movement: \n  w - up \n  a - left \n  s - down \n  d - right \n  x - to exit \npress enter to continue...');
+        while (myField.lastMove != 'x') myField.update();
     }
     print(){
+        this.grid[this.curPos[0]][this.curPos[1]] = '*';
         for (let row in this.grid){
             console.log(this.grid[row].join(''));
         }
+        if (!this.lastMoveValid){
+            console.log(`Can't go that way...`)
+        }
     }
     playerMovement(){
-        console.log(`prevPos: ${this.prevPos}`)
         console.log(`curPos: ${this.curPos}`)
         console.log(`grid height: ${this.grid.length}`)
         console.log(`grid width: ${this.grid[0].length}`)
             
-        const move = prompt('Which way?');
+        this.lastMoveValid = true;
+        const move = prompt('Which way? ');
+        this.lastMove = move;
         switch (move){
             case 'w': {
-                this.nextPos[0] = this.curPos[0] - 1;
+                this.curPos[0] = this.curPos[0] - 1;
+                break;
+            }
+            case 'a': {
+                this.curPos[1] = this.curPos[1] - 1;
+                break;
+            }
+            case 's': {
+                this.curPos[0] = this.curPos[0] + 1;
+                break;
+            }
+            case 'd': {
+                this.curPos[1] = this.curPos[1] + 1;
                 break;
             }
             default : {
-                console.log('no input received')
             }
         }
-        if(this.nextPos[0] > 0 && this.nextPos[0] < this.grid.length && this.nextPos[1] > 0 && this.nextPos[1] < this.grid[0].length) {
-            this.curPos = this.nextPos;
+        if(this.curPos[0] < 0){
+            this.curPos[0] += 1;
+            this.lastMoveValid = false;
+        }
+        else if(this.curPos[0] > this.grid.length - 1){
+            this.curPos[0] -= 1;
+            this.lastMoveValid = false;
+        }
+        else if(this.curPos[1] < 0){
+            this.curPos[1] += 1;
+            this.lastMoveValid = false;
+        }
+        else if(this.curPos[1] > this.grid[0].length - 1){
+            this.curPos[1] -= 1;
+            this.lastMoveValid = false;
         }
     }
     update(){
         console.clear();
-            this.grid[this.curPos[0]][this.curPos[1]] = '*';
-            this.prevPos = this.curPos;
-
         this.print();
         this.playerMovement();
 
@@ -54,13 +83,12 @@ class Field {
 }
 
 const myField = new Field([
-  ['*', '░', 'O', '░', 'O', '░', 'O', '░', '░', 'O', '░'],
+  ['O', '░', 'O', '░', 'O', '░', 'O', '░', '░', 'O', '░'],
   ['░', 'O', '░', '░', 'O', '░', 'O', '░', '░', 'O', '░'],
   ['░', '^', '░','░', 'O', '░', 'O', '░', '░', 'O', '░'],
   ['░', 'O', '░', '░', 'O', '░', 'O', '░', '░', 'O', '░'],
   ['░', '^', '░','░', 'O', '░', 'O', '░', '░', 'O', '░']
 ]);
-myField.startNotice();
-myField.print();
-myField.playerMovement();
-while (0 != 1) myField.update();
+
+
+myField.start();
