c89be056d42c4a161012d7f210200f9c89e230e3
aronKleinhans <kleinhans.aron@gmail.com>
2021-04-25 16:08:19 +0200

    add death/restart

--- main.js
+++ main.js
@@ -7,35 +7,50 @@ const pathCharacter = '*';
 
 class Field {
     constructor(grid){
-    this.grid = grid;
-    this._startPos = [3, 0];
-    this.curPos = this._startPos;
-    this.lastMove = '';
-    this.lastMoveValid = true;
+        [...this.grid] = [...grid];
+        this.oGrid = [];
+        this._startPos = [0, 0];
+        this.curPos = [0, 0];
+        this.lastMove = '';
+        this.lastMoveValid = true;
     }
-    
+    copyGrid(base, target){
+        let x = [];
+      
+        for( let i = 0; i < base.length; i++){
+            for( let j = 0; j < base[i].length; j++){
+                x.push(base[i][j])
+            }
+            target.push(x);
+            x = [];
+        }
+    }
     start(){
         console.clear();
         const notice = prompt('Welcome to Hatfinder! \n \nUse these for movement: \n  w - up \n  a - left \n  s - down \n  d - right \n  x - to exit \npress enter to continue...');
-        while (myField.lastMove != 'x') myField.update();
+        this.copyGrid(this.grid, this.oGrid);
+        this.update();
     }
     print(){
+        console.log(this.grid)
+        console.log('-----')
+        console.log(this.oGrid)
+        console.log('-----')
+        console.log(field)
         this.grid[this.curPos[0]][this.curPos[1]] = '*';
         for (let row in this.grid){
             console.log(this.grid[row].join(''));
         }
-        if (!this.lastMoveValid){
-            console.log(`Can't go that way...`)
-        }
     }
     playerMovement(){
+        console.log(this._startPos)
         console.log(`curPos: ${this.curPos}`)
         console.log(`grid height: ${this.grid.length}`)
         console.log(`grid width: ${this.grid[0].length}`)
-            
         this.lastMoveValid = true;
         const move = prompt('Which way? ');
         this.lastMove = move;
+        
         switch (move){
             case 'w': {
                 this.curPos[0] = this.curPos[0] - 1;
@@ -74,21 +89,43 @@ class Field {
         }
     }
     update(){
-        console.clear();
-        this.print();
-        this.playerMovement();
+        while (myField.lastMove != 'x' && this.lastMoveValid) {
+            console.clear();
+            this.print();
+            this.playerMovement();
+        }
+        if (!this.lastMoveValid){
+            this.restart();
+        }
 
     }
-    
+    restart() {
+        console.clear();
+        [...this.curPos] = [...this._startPos];
+        this.grid = [];
+        this.copyGrid(this.oGrid, this.grid);
+        
+        console.log(`You died...`);
+        let ans = prompt('Do you want to try again?(y/n)');
+        
+        if (ans === 'y') {
+            this.lastMoveValid = true;
+            this.update();
+        }
+        
+    }    
 }
+const field = [
+    ['O', '░', 'O', '░', 'O', '░', 'O', '░', '░', 'O', '░'],
+    ['░', 'O', '░', '░', 'O', '░', 'O', '░', '░', 'O', '░'],
+    ['░', '^', '░','░', 'O', '░', 'O', '░', '░', 'O', '░'],
+    ['░', 'O', '░', '░', 'O', '░', 'O', '░', '░', 'O', '░'],
+    ['░', '^', '░','░', 'O', '░', 'O', '░', '░', 'O', '░']
+    ];
 
-const myField = new Field([
-  ['O', '░', 'O', '░', 'O', '░', 'O', '░', '░', 'O', '░'],
-  ['░', 'O', '░', '░', 'O', '░', 'O', '░', '░', 'O', '░'],
-  ['░', '^', '░','░', 'O', '░', 'O', '░', '░', 'O', '░'],
-  ['░', 'O', '░', '░', 'O', '░', 'O', '░', '░', 'O', '░'],
-  ['░', '^', '░','░', 'O', '░', 'O', '░', '░', 'O', '░']
-]);
+
+
+const myField = new Field(field);
 
 
 myField.start();
