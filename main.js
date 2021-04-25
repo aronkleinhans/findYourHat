f3a52f42f7a15b20dae35729a4024470d820dc8a
aronKleinhans <kleinhans.aron@gmail.com>
2021-04-25 11:40:20 +0200

    muh brain

--- main.js
+++ main.js
@@ -6,20 +6,61 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-  constructor(grid){
+    constructor(grid){
     this.grid = grid;
-  }
- print(){
+    this.curPos = [0, 0];
+    this.nextPos = [0, 0];
+    }
+    
+    startNotice(){
+        console.clear();
+        const notice = prompt('Welcome to Hatfinder! \n \n Use these for movement: \n w - up \n a - left \n s - down \n d - right \n \n press enter to continue...')
+    }
+    print(){
+        for (let row in this.grid){
+            console.log(this.grid[row].join(''));
+        }
+    }
+    playerMovement(){
+        console.log(`prevPos: ${this.prevPos}`)
+        console.log(`curPos: ${this.curPos}`)
+        console.log(`grid height: ${this.grid.length}`)
+        console.log(`grid width: ${this.grid[0].length}`)
+            
+        const move = prompt('Which way?');
+        switch (move){
+            case 'w': {
+                this.nextPos[0] = this.curPos[0] - 1;
+                break;
+            }
+            default : {
+                console.log('no input received')
+            }
+        }
+        if(this.nextPos[0] > 0 && this.nextPos[0] < this.grid.length && this.nextPos[1] > 0 && this.nextPos[1] < this.grid[0].length) {
+            this.curPos = this.nextPos;
+        }
+    }
+    update(){
+        console.clear();
+            this.grid[this.curPos[0]][this.curPos[1]] = '*';
+            this.prevPos = this.curPos;
 
-   for (let row in this.grid){
-     console.log(this.grid[row].join(''));
-   }
- }
+        this.print();
+        this.playerMovement();
+
+    }
+    
 }
 
 const myField = new Field([
-  ['*', '░', 'O'],
-  ['░', 'O', '░'],
-  ['░', '^', '░'],
+  ['*', '░', 'O', '░', 'O', '░', 'O', '░', '░', 'O', '░'],
+  ['░', 'O', '░', '░', 'O', '░', 'O', '░', '░', 'O', '░'],
+  ['░', '^', '░','░', 'O', '░', 'O', '░', '░', 'O', '░'],
+  ['░', 'O', '░', '░', 'O', '░', 'O', '░', '░', 'O', '░'],
+  ['░', '^', '░','░', 'O', '░', 'O', '░', '░', 'O', '░']
 ]);
+myField.startNotice();
 myField.print();
+myField.playerMovement();
+while (0 != 1) myField.update();
